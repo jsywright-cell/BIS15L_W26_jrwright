a39f9d65b63dbfde02ac960b6720fe5d9845cac7
jsywright-cell <jsywright@ucdavis.edu>
2026-02-27 15:38:45 -0800

    update lab 14/15

---
+++ lab15/elephants_app.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+library(janitor)
+library(shiny)
+library(shinydashboard)
+
+elephants <- read_csv("data/elephants_data/elephants.csv") %>%
+  clean_names()
+
+ui <- dashboardPage(
+  
+  dashboardHeader(title="Age and Height of Elephants by Sex"),
+  
+  dashboardSidebar(
+    
+    selectInput("y",
+                "Select Elephant Variable",
+                choices=c("age", "height"),
+                selected="age")),
+  
+  dashboardBody(
+    
+    plotOutput("plot", width="600px", height="500px")
+  )
+  
+)
+
+server <- function(input, output, session) {
+  
+  output$plot <- renderPlot({
+    
+    elephants %>% 
+      ggplot(aes(x = sex,
+                 y = .data[[input$y]],
+                 fill = sex))+
+      geom_boxplot(alpha=0.75)+
+      labs(title="Elephant Variables by Sex",
+           x="Sex",
+           fill="Sex")+
+      theme_linedraw()
+    
+    
+  })
+  
+}
+
+shinyApp(ui, server)
